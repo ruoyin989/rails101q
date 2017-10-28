f07cdb84c767ca969be4ee1944d1fb52acee5240
ruoyin989 <liu199810989@gmail.com>
2017-10-28 17:07:56 +0800

    move to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,6 @@ end
 
 def edit
   @group =Group.find(params[:id])
-
 end
 
   def new
@@ -18,15 +17,26 @@ end
 
   def create
     @group =Group.new(group_params)
-    @group.save
-
+    if @group.save
     redirect_to groups_path
+  else
+    render :new
+  end
   end
 
   def update
     @group =Group.find(params[:id])
-    @group.update(group_params)
+    if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
+  else
+    render :edit 
+  end
+  end
+
+  def destroy
+    @group=Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
   end
 
   private
